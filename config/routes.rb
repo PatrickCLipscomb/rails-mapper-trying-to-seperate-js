0cdf1467b494a8478d6082733e46c72b68a612cb
patrick lipscomb <patrick74lipscomb@gmail.com>
2016-12-15 14:41:57 -0800

    prepare for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "home#index"
+  root "locations#index"
   resources :locations
   resources :crawls
   # match "/crawl/" => "locations#crawl", :via => :get, :as => :crawl
