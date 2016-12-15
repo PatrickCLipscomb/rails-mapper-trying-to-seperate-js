04bbb80f62e9b28512018f9e160e895cfa6afcfa
patrick lipscomb <patrick74lipscomb@gmail.com>
2016-12-15 10:09:08 -0800

    it will add the beginnings of the datamap

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,12 @@
 class Location < ApplicationRecord
+
+  def number_of_bars
+    holder = 0
+    Location.all.each do |loc|
+      if loc.category == "Bar"
+        holder += 1
+      end
+    end
+    holder
+  end
 end
