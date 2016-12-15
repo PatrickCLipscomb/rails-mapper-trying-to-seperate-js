af26d2bf0158e6da6dd257cc495298634c1fa118
patrick lipscomb <patrick74lipscomb@gmail.com>
2016-12-15 14:51:05 -0800

    add database changes for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 20161215204356) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "crawls", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "stops"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "stops",      default: [],              array: true
   end
 
   create_table "locations", force: :cascade do |t|
